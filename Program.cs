ec68eba0a2131a1d8323457ecd3d7c14cdcc8fc8
DOLLAR1990 <DOLLAR1990@mail.ru>
2022-07-18 12:48:42 +0300

    add task21

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int getRandomNumber(int minBorder, int maxBorder)
+{
+   return new Random().Next(minBorder, maxBorder + 1);
+}
+int getMaxDigit(int number)
+{
+        int maxDigit = 0;
+        int digitOne = number / 10;
+        int digitTwo = number % 10;
+        maxDigit = digitOne;
+        if(maxDigit < digitTwo)
+        {
+            maxDigit = digitTwo;
+        }
+    return maxDigit;
+}
+int randomNumber = getRandomNumber(10,99);
+int maxDigit = getMaxDigit (randomNumber);
+Console.WriteLine($" Наибольшая цифра числа {randomNumber} это {maxDigit}");
